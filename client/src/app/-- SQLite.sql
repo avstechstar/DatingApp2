bbd2be37ca3515ab7656381ef0bb2b6c8132b7cd
Harry <avstechstar@gmail.com>
2021-08-05 12:25:26 -0500

    End of seciton 3

---
+++ client/src/app/-- SQLite.sql
@@ -0,0 +1,13 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Bob");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Tom");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Jane");
+
+UPDATE Users
+SET UserName = 'Harry'
+WHERE Id=1
