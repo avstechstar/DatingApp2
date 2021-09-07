cda9baa4eea7e62b655cd8e6aa3fe46e5baf80e3
Harry <avstechstar@gmail.com>
2021-09-07 11:07:55 -0500

    End of Seciton 9 recreated

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,22 @@
+import { Photo } from "./Photo";
+
+
+    export interface Member {
+        id: number;
+        username: string;
+        photoUrl: string;
+        age: number;
+        knownAs: string;
+        created: Date;
+        lastActive: Date;
+        lookingFor: string;
+        gender: string;
+        introduction: string;
+        interests: string;
+        city: string;
+        country: string;
+        photos: Photo[];
+    }
+
+
+
